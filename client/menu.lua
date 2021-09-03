0a38c74aa5a777f4c00118e2fcb7cc640cbcab0e
Agent BUB <46576969+AgentBUB@users.noreply.github.com>
2021-09-03 14:14:06 -0400

    Update: Menu Open at Map Maker (Version 2)

--- client/menu.lua
+++ client/menu.lua
@@ -11,8 +11,7 @@
         contact ASP first via Discord or our contact form on the website.
 
 ───────────────────────────────────────────────────────────────
-]]
-
+]] 
 _menuPool = NativeUI.CreatePool()
 
 local MenuOri = 0
@@ -42,11 +41,15 @@ function ShowNotification(text)
     DrawNotification(false, false)
 end
 
-function ExtraChanger(ped, vehicle, menu)    
-	local veh_extras = {['vehicleExtras'] = {}}
-    local items = {['vehicle'] = {}}
-    
-	for extraID = 0, 20 do
+function ExtraChanger(ped, vehicle, menu)
+    local veh_extras = {
+        ['vehicleExtras'] = {}
+    }
+    local items = {
+        ['vehicle'] = {}
+    }
+
+    for extraID = 0, 20 do
         if DoesExtraExist(vehicle, extraID) then
             veh_extras.vehicleExtras[extraID] = (IsVehicleExtraTurnedOn(vehicle, extraID) == 1)
         end
@@ -55,18 +58,20 @@ function ExtraChanger(ped, vehicle, menu)
     for _, CustomExtra in pairs(Config.CustomNames) do
         for k, v in pairs(veh_extras.vehicleExtras) do
             if GetEntityModel(vehicle) == GetHashKey(CustomExtra.vehicle) then
-                x = ""..k..""
-                local extraItem = NativeUI.CreateCheckboxItem(CustomExtra.extra[x], veh_extras.vehicleExtras[k],"Toggle for "..CustomExtra.extra[x].." | Extra #"..k)
+                x = "" .. k .. ""
+                local extraItem = NativeUI.CreateCheckboxItem(CustomExtra.extra[x], veh_extras.vehicleExtras[k],
+                    "Toggle for " .. CustomExtra.extra[x] .. " | Extra #" .. k)
                 mainMenu:AddItem(extraItem)
-                items.vehicle[k] = extraItem 
+                items.vehicle[k] = extraItem
             else
-                local extraItem = NativeUI.CreateCheckboxItem('Extra ' .. k, veh_extras.vehicleExtras[k],"Toggle for Extra "..k)
+                local extraItem = NativeUI.CreateCheckboxItem('Extra ' .. k, veh_extras.vehicleExtras[k],
+                    "Toggle for Extra " .. k)
                 mainMenu:AddItem(extraItem)
                 items.vehicle[k] = extraItem
             end
-        end 
+        end
     end
-    
+
     mainMenu.OnCheckboxChange = function(sender, item, checked)
         for k, v in pairs(items.vehicle) do
             if item == v then
@@ -82,57 +87,116 @@ function ExtraChanger(ped, vehicle, menu)
 end
 
 function CreditsSection(ped, vehicle, menu)
-    local submenu = _menuPool:AddSubMenu(menu, "Menu Info / Credits", "Information about the ~y~Extra Menu ~w~and the creators.", true, true)
-    submenu:AddItem(NativeUI.CreateItem(
-        "Menu Information", "The ~y~Extra Menu ~w~was created to make changing extras easier, and to allow for custom names to be added for extras for custom vehicles in servers."
-    ))
-    submenu:AddItem(NativeUI.CreateItem(
-        "Creators Information", "This menu was created by ~r~Agent Squad Productions~w~."
-    ))
-    submenu:AddItem(NativeUI.CreateItem(
-        "Links", "~o~agentsquad.org ~w~| ~b~discord.agentsquad.org"
-    ))
+    local submenu = _menuPool:AddSubMenu(menu, "Menu Info / Credits",
+        "Information about the ~y~Extra Menu ~w~and the creators.", true, true)
+    submenu:AddItem(NativeUI.CreateItem("Menu Information",
+        "The ~y~Extra Menu ~w~was created to make changing extras easier, and to allow for custom names to be added for extras for custom vehicles in servers."))
+    submenu:AddItem(NativeUI.CreateItem("Creators Information",
+        "This menu was created by ~r~Agent Squad Productions~w~."))
+    submenu:AddItem(NativeUI.CreateItem("Links", "~o~agentsquad.org ~w~| ~b~discord.agentsquad.org"))
     submenu:SetMenuWidthOffset(Config.MenuWidth)
 end
 
-
 function openMenu(ped, vehicle)
     if mainMenu ~= nil and mainMenu:Visible() then
-		mainMenu:Visible(false)
-		return
-	end
+        mainMenu:Visible(false)
+        return
+    end
     mainMenu = NativeUI.CreateMenu(MenuTitle, "~b~The easy way to change extras!", MenuOri)
     _menuPool:Add(mainMenu)
     ExtraChanger(ped, vehicle, mainMenu)
     if Config.EnableCredits then
         CreditsSection(ped, vehicle, mainMenu)
     end
-    
+
     mainMenu:SetMenuWidthOffset(Config.MenuWidth)
-    
+
     _menuPool:RefreshIndex()
-    _menuPool:MouseControlsEnabled (false);
-	_menuPool:MouseEdgeEnabled (false);
-	_menuPool:ControlDisablingEnabled(false);
+    _menuPool:MouseControlsEnabled(false);
+    _menuPool:MouseEdgeEnabled(false);
+    _menuPool:ControlDisablingEnabled(false);
 end
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(0)
-        _menuPool:ProcessMenus()
-        local ped = GetPlayerPed(-1)
-        local vehicle = GetVehiclePedIsIn(ped, false)
-        if IsControlJustReleased(1, Config.MenuKey) then
-			if IsPedInAnyVehicle(ped, false) and GetPedInVehicleSeat(vehicle, -1) == ped then
-                openMenu(ped, vehicle)
-                mainMenu:Visible(not mainMenu:Visible())
-			end
+        if Config.locationOpen == false then
+            Citizen.Wait(0)
+            _menuPool:ProcessMenus()
+            local ped = GetPlayerPed(-1)
+            local vehicle = GetVehiclePedIsIn(ped, false)
+            if IsControlJustReleased(1, Config.MenuKey) then
+                if IsPedInAnyVehicle(ped, false) and GetPedInVehicleSeat(vehicle, -1) == ped then
+                    openMenu(ped, vehicle)
+                    mainMenu:Visible(not mainMenu:Visible())
+                end
+            end
+
+            if IsPedInAnyVehicle(ped, false) == false then
+                if mainMenu ~= nil and mainMenu:Visible() then
+                    mainMenu:Visible(false)
+                end
+            end
         end
-		
-		if IsPedInAnyVehicle(ped, false) == false then
-			if mainMenu ~= nil and mainMenu:Visible() then
-				mainMenu:Visible(false)
-			end
-		end
+        if Config.locationOpen == true then
+            Citizen.Wait(5)
+            local player = GetPlayerPed(-1)
+            local playerLoc = GetEntityCoords(player)
+
+            for _, location in ipairs(Config.positions) do
+                teleport_text = location[3]
+                loc = {
+                    x = location[1][1],
+                    y = location[1][2],
+                    z = location[1][3],
+                    heading = location[1][4]
+                }
+                Red = location[2][1]
+                Green = location[2][2]
+                Blue = location[2][3]
+
+                DrawMarker(1, loc.x, loc.y, loc.z, 0, 0, 0, 0, 0, 0, 1.501, 1.5001, 0.5001, Red, Green, Blue, 200, 0, 0,
+                    0, 0)
+
+                if CheckPos(playerLoc.x, playerLoc.y, playerLoc.z, loc.x, loc.y, loc.z, 2) then
+                    alert(teleport_text)
+                    _menuPool:ProcessMenus()
+                    local ped = GetPlayerPed(-1)
+                    local vehicle = GetVehiclePedIsIn(ped, false)
+                    if IsControlJustReleased(1, Config.MenuKey) then
+                        if IsPedInAnyVehicle(ped, false) and GetPedInVehicleSeat(vehicle, -1) == ped then
+                            openMenu(ped, vehicle)
+                            mainMenu:Visible(not mainMenu:Visible())
+                        end
+                    end
+
+                    if IsPedInAnyVehicle(ped, false) == false then
+                        if mainMenu ~= nil and mainMenu:Visible() then
+                            mainMenu:Visible(false)
+                        end
+                    end
+                end
+            end
+        end
     end
 end)
+
+if Config.locationOpen == true then
+    function CheckPos(x, y, z, cx, cy, cz, radius)
+        local t1 = x - cx
+        local t12 = t1 ^ 2
+
+        local t2 = y - cy
+        local t21 = t2 ^ 2
+
+        local t3 = z - cz
+        local t31 = t3 ^ 2
+
+        return (t12 + t21 + t31) <= radius ^ 2
+    end
+
+    function alert(msg)
+        SetTextComponentFormat("STRING")
+        AddTextComponentString("~"..keys[Config.MenuKey].."~ | ~w~"..msg)
+        DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+    end
+end
