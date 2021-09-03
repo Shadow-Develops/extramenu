1cfe41e9c54c2ffea9d75f1041df429e6e9576ad
Agent BUB <46576969+AgentBUB@users.noreply.github.com>
2021-09-03 14:15:18 -0400

    Update: Menu Open at Map Maker (Version 2)

---
+++ keys.lua
@@ -0,0 +1,33 @@
+--[[
+───────────────────────────────────────────────────────────────
+
+	Extra Changer Menu (keys.lua) - Created by Agent Squad Prodcutions
+	
+	This file is so that keys appear properly when useing Config.locationOpen.
+    Do not change this file unless you are adding in a new key that isn't defined.
+
+    Follow this format:
+        key[KEY INDEX] = "CONTROL NAME"
+    
+    You can find all key indexes / names at: https://docs.fivem.net/docs/game-references/controls/
+
+───────────────────────────────────────────────────────────────
+]]
+keys = {}
+
+keys[311] = "INPUT_REPLAY_SHOWHOTKEY"
+keys[244] = "INPUT_INTERACTION_MENU"
+keys[77] = "INPUT_VEH_HOTWIRE_LEFT"
+keys[20] = "INPUT_MULTIPLAYER_INFO"
+keys[39] = "INPUT_SNIPER_ZOOM"
+keys[40] = "INPUT_SNIPER_ZOOM_IN_ONLY"
+keys[19] = "INPUT_CHARACTER_WHEEL"
+keys[168] = "INPUT_SELECT_CHARACTER_TREVOR"
+keys[288] = "INPUT_REPLAY_START_STOP_RECORDING"
+keys[289] = "INPUT_REPLAY_START_STOP_RECORDING_SECONDARY"
+keys[170] = "INPUT_SAVE_REPLAY_CLIP"
+keys[166] = "INPUT_SELECT_CHARACTER_MICHAEL"
+keys[167] = "INPUT_SELECT_CHARACTER_FRANKLIN"
+keys[56] = "INPUT_DROP_WEAPON"
+keys[57] = "INPUT_DROP_AMMO"
+keys[344] = "INPUT_SWITCH_VISOR"
