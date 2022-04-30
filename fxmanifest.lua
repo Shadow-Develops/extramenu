fd1d6fde169dfc9f9c0d01c4ce8d71ddfba50af6
Agent BUB <zzhines@gmail.com>
2022-04-30 06:22:47 -0400

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version ‘cerulean’
+game ‘gta5’
+
+name 'Extra Menu'
+description 'A simple menu to make changing vehicle extras easy with the ability to customize the names extras have within the menu for certain vehicles.'
+author 'Shadow Development (www.shadowdevs.com)'
+version 'v2.0.0'
+url 'https://github.com/Shadow-Develops/extramenu'
+
+client_scripts {
+    'NativeUI.lua',
+    'keys.lua',
+    'client/menu.lua'
+}
+
+server_scripts {
+    'keys.lua',
+}
+
+shared_scripts {
+    'config.lua',
+}
